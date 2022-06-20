da2ace61a381f40e750125c2b5f3bd02d119e984
mattysaints <tbarbera98@gmail.com>
2022-06-21 01:16:50 +0200

    Modifiche Class User

--- HerculesWallet/app/src/main/java/com/example/herculeswallet/model/User.kt
+++ HerculesWallet/app/src/main/java/com/example/herculeswallet/model/User.kt
@@ -2,9 +2,26 @@ package com.example.herculeswallet.model
 
 import java.util.prefs.AbstractPreferences
 
-data class User (val email: String, val password: String, var wallet: ArrayList<Crypto>, var preferences: ArrayList<String>){
+data class User (val email: String, val password: String, var wallet: HashMap<Crypto,String>, var preferences: ArrayList<String>){
 
     override fun toString(): String {
         return "User(email='$email', password='$password', wallet=$wallet, preferences=$preferences)"
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as User
+
+        if (wallet != other.wallet) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return wallet.hashCode()
+    }
+
+
 }
