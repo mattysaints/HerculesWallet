a0a96437eb8582a2352b11c697bf9237cda622e2
ChrisQuara <chrisquara99@icloud.com>
2022-07-04 22:39:58 +0200

    Fixup! Logout ora funzionante

--- HerculesWallet/app/src/main/java/com/example/herculeswallet/view/OptionsFragment.kt
+++ HerculesWallet/app/src/main/java/com/example/herculeswallet/view/OptionsFragment.kt
@@ -1,21 +1,20 @@
 package com.example.herculeswallet.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.example.herculeswallet.R
-import com.example.herculeswallet.databinding.FragmentOptionsBinding
 import com.example.herculeswallet.model.User
 import com.example.herculeswallet.viewmodels.MainViewModel
 
 
 class OptionsFragment : Fragment(R.layout.fragment_options) {
-
-    private lateinit var binding: FragmentOptionsBinding
     private val model : MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,11 +26,6 @@ class OptionsFragment : Fragment(R.layout.fragment_options) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentOptionsBinding.inflate(layoutInflater)
-        binding.buttonlogout.setOnClickListener() {
-            println("Hola")
-            model.signOut()
-        }
         return super.onCreateView(inflater, container, savedInstanceState)
     }
 
@@ -42,6 +36,14 @@ class OptionsFragment : Fragment(R.layout.fragment_options) {
                     onStop()
                 }
             })
+
+        val bottone: Button = view.findViewById(R.id.buttonlogout)
+        bottone.setOnClickListener(){
+            onDestroy()
+            model.signOut()
+            model.userMutableLiveData.value = null
+            startActivity(Intent(context, MainActivity::class.java))
+        }
     }
 
 }
