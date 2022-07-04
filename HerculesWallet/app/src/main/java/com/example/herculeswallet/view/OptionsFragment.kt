c3bc2023999fa6249ab9d5b1edb81a72bd304f6a
ChrisQuara <chrisquara99@icloud.com>
2022-07-04 22:01:14 +0200

    Ho provato a far funzionare il logout ma l'onclick non parte

--- HerculesWallet/app/src/main/java/com/example/herculeswallet/view/OptionsFragment.kt
+++ HerculesWallet/app/src/main/java/com/example/herculeswallet/view/OptionsFragment.kt
@@ -1,35 +1,47 @@
 package com.example.herculeswallet.view
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import com.example.herculeswallet.R
-import com.example.herculeswallet.databinding.ActivityMainBinding
+import com.example.herculeswallet.databinding.FragmentOptionsBinding
 import com.example.herculeswallet.model.User
 import com.example.herculeswallet.viewmodels.MainViewModel
 
 
 class OptionsFragment : Fragment(R.layout.fragment_options) {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: FragmentOptionsBinding
     private val model : MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+    }
 
-        model.userMutableLiveData.observe(this,
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentOptionsBinding.inflate(layoutInflater)
+        binding.buttonlogout.setOnClickListener() {
+            println("Hola")
+            model.signOut()
+        }
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        model.userMutableLiveData.observe(viewLifecycleOwner,
             Observer<User?> { user ->
                 if (user == null) {
                     onStop()
-                    //startActivity(Intent(this, MainActivity::class.java))
                 }
             })
-
-        binding.buttonAccedi.setOnClickListener {
-            model.signOut()
-        }
     }
 
 }
